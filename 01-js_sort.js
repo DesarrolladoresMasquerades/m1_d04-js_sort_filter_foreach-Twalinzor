b10df0f0c7d263765ccc2d3937ba05df4ca6a240
Stan Tapchylka <stanislautapchylka@gmail.com>
2022-01-20 11:31:48 +0100

    Done

--- 01-js_sort.js
+++ 01-js_sort.js
@@ -1,1 +1,49 @@
 // Class code examples
+
+const numbers = [22, 23, 99, 68, 1, 0, 9, 112, 223, 64, 18];
+
+console.log("BEFORE: ", numbers);
+
+numbers.sort();
+
+console.log("AFTER: ", numbers);
+
+console.log(typeof numbers[0]); // Elements stay as numbers
+
+console.log(numbers === numbers.sort()); // Array stays in the same "box"
+
+// Passing sorting criteria using a function
+
+// Rules are that TWO ELEMENTS will be given to your function and your function must return -1, 0 or 1, according to whether element 1 is smaller, equal or greater than element 2
+
+function sortMyNumbers(element1, element2) {
+  if (element1 > element2) return -1;
+  if (element1 === element2) return 0;
+  if (element1 < element2) return 1;
+}
+
+function quickSort(el1, el2) {
+  return el1 - el2;
+}
+
+const sortedNumbers = numbers.sort(quickSort);
+
+console.log("sortedNumbers: ", sortedNumbers);
+
+const students = [
+  { firstName: "John", lastName: "Carr", age: 34 },
+  { firstName: "Leonardo", lastName: "Di Vittorio", age: 21 },
+  { firstName: "Sarah", lastName: "O'Connor", age: 45 },
+];
+
+function sortStudentsAge(stud1, stud2) {
+  if (stud1.age < stud2.age) return -1;
+  if (stud2.age === stud2.age) return 0;
+  if (stud3.age > stud3.age) return 1;
+}
+
+function sortStudentsAgeQuick(stud1, stud2) {
+  return parseInt(stud1.age) - parseInt(stud2.age);
+}
+
+console.log("students by age: ", students.sort(sortStudentsAge));
--- exercies/01-exe-js_sort.js
+++ exercies/01-exe-js_sort.js
@@ -2,28 +2,33 @@
 
 //Exercise 1: Using the array (arrOfStrings), sort an array from shortest string to the longest.
 // DO NOT mutate the original array
-const arrOfStrings = ['cat', 'wolf', 'yo', 'animal'];
+const arrOfStrings = ["cat", "wolf", "yo", "animal"];
 
 const longestStr = (arr) => {
-   //your code...
-}
+  function sortWords(word1, word2) {
+    if (word1.length < word2.length) return -1;
+    if (word1.length === word2.length) return 0;
+    if (word1.length > word2.length) return 1;
+  }
+  return arr.sort(sortWords);
+};
 
-let longestStrings = longestStr(arrOfStrings)
-console.log(longestStrings)
+let longestStrings = longestStr(arrOfStrings);
+console.log(longestStrings);
 
 //ANSWER should be ['yo', 'cat', 'wolf', 'animal']
 
 //Exercise 2: Using the same arr (arrOfStrings), sort its elements alphabetically.
 // DU NOT mutate the original array
 
-const arrOfStrings = ['cat', 'wolf', 'yo', 'animal'];
+const arrOfStrings2 = ["cat", "wolf", "yo", "animal"];
 
 const sortedStr = (arr) => {
-   //your code...
-}
+  return arr.sort();
+};
 
-let sortedStrings = sortedStr(arrOfStrings)
-console.log(sortedStrings)
+let sortedStrings = sortedStr(arrOfStrings2);
+console.log(sortedStrings);
 
 //ANSWER should be  ['animal', 'cat', 'wolf', 'yo']
 
@@ -32,21 +37,21 @@ console.log(sortedStrings)
 // Please do not mutate the original array
 
 const people = [
-  { name: 'Candice', age: 25 },
-  { name: 'Tammy', age: 30 },
-  { name: 'Allen', age: 19 },
-  { name: 'Nettie', age: 21 },
-  { name: 'Stuart', age: 30 },
-  { name: 'Bill', age: 19 }
+  { name: "Candice", age: 25 },
+  { name: "Tammy", age: 30 },
+  { name: "Allen", age: 19 },
+  { name: "Nettie", age: 21 },
+  { name: "Stuart", age: 30 },
+  { name: "Bill", age: 19 },
 ];
 
 const sortByAge = (arr) => {
-   //your code...
-}
+  //your code...
+};
 
-let output = sortByAge(people)
-console.log(output)
- 
+let output = sortByAge(people);
+console.log(output);
+
 /* ANSWER should be
  [
   { name: 'Bill', age: 19 },
